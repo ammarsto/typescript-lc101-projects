3c8b92f542a506d5265873eb29dce485d55bc41d
Amy Stockhausen <stockhausenamy@gmail.com>
2022-12-01 09:11:03 -0600

    Finished Exercise

--- exercises/parts1-5.js
+++ exercises/parts1-5.js
@@ -1,13 +1,38 @@
-// URL for the instructions: 
-// https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
+"use strict";
+exports.__esModule = true;
 // Part 1: Declare (5) Variables With Type
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
 // Part 2: Print Days to Mars
+// let milesToMars: number = kilometersToMars * milesPerKilometer;
+// let hoursToMars: number = milesToMars / speedMph;
+// let daysToMars: number = hoursToMars / 24;
 // Code an output statement here (use a template literal):
 // Part 3: Create a Function ("getDaysToLocation")
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
+// console.log(`${spacecraftName} will take ${getDaysToLocation(kilometersToMars)} days to reach Mars and ${getDaysToLocation(kilometersToTheMoon)} days to reach the moon.`);
 // Part 4: Create a Spacecraft Class
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(name, speedMph) {
+        this.milesPerKilometer = .621;
+        this.name = name;
+        this.speedMph = speedMph;
+    }
+    Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
+        var milesAway = kilometersAway * this.milesPerKilometer;
+        var hoursAway = milesAway / this.speedMph;
+        return hoursAway / 24;
+    };
+    Spacecraft.prototype.printDaysToLocation = function (location) {
+        console.log("".concat(this.name, " would take ").concat(this.getDaysToLocation(location.kilometersAway), " days to get to ").concat(location.name, "."));
+    };
+    return Spacecraft;
+}());
 // Create an instance of the class here:
+var spaceShuttle = new Spacecraft('Determination', 17500);
+console.log("It will take ".concat(spaceShuttle.getDaysToLocation(kilometersToMars), " days for ").concat(spaceShuttle.name, " to reach its destination."));
+console.log("It will take ".concat(spaceShuttle.getDaysToLocation(kilometersToTheMoon), " days for ").concat(spaceShuttle.name, " to reach its destination."));
 // Move your output statements from part 3 here. Update the template literals use the
 // instance of the class.
 // Part 5: Export and Import the SpaceLocation Class
